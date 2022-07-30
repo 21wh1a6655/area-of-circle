4c48073328607732f265cf3212bafd51da1a5fde
21wh1a6655 <96616743+21wh1a6655@users.noreply.github.com>
2022-07-30 18:29:37 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,8 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def area_is(r):
+    area = 3.14 * r * r
+    return area
+
+area_is(5)
+78.5
